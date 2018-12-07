5805189fb37b1b91abf8dc45061762aa13e795ff
Angus Williams <aw16797@my.bristol.ac.uk>
2018-12-07 21:41:31 +0000

    fixed?

--- face.cpp
+++ face.cpp
@@ -13,73 +13,98 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
-#include <stdio.h>
+#include <string>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 using namespace cv;
 
 /** Function Headers */
-void detectAndDisplay( Mat frame, int** coordinates );
+void detectAndDisplay( Mat frame );
+int toInt( char x );
 float f_one();
 
 /** Global variables */
 String cascade_name = "frontalface.xml";
 CascadeClassifier cascade;
 int tp,truefaces,detected;
+int coordinates[11][4];
 
 /** @function main */
-int main( int argc, const char** argv )
+int main( int argc, char** argv )
 {
   // 1. Read Input Image
-  Mat frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+  Mat frame = imread("images/dart15.jpg", CV_LOAD_IMAGE_COLOR);
+	char coords[11][4];
+  std::ifstream file("csvs/dart15face.csv");
 
-	int i = 0;
-	char[11][4] coords;
+  for(int a = 0; a < 11; a++)
+  {
+    std::string line;
+    std::getline (file,line);
+    // if(!file.good())
+    // {
+    //   break;
+    // }
+    std::stringstream iss(line);
 
- 	while (getline(argv[2], coords[i][0] ',')) {
-	getline(argv[2],coords[i][1]);
-	getline(argv[2],coords[i][2]);
-	getline(argv[2],coords[i][3]);
- 	i++;
-	}
+    for(int b = 0; b < 4; b++)
+    {
+      std::string value;
+      std::getline(iss,value,',');
+      // if(!iss.good())
+      // {
+      //   break;
+      // }
+      std::stringstream convertor(value);
+      convertor >> coordinates[a][b];
+      std::cout << coordinates[a][b] << std::endl;
+      //std::cout << coordinates[a][b] << std::endl;
+    }
+  }
 
-	int truefaces = i;
+  int foo = 1;
+  int i = 0;
+  while(foo != 0){
+    foo = coordinates[i][0];
+    
+  }
+
+
+	truefaces = i;
 	int intcoords [truefaces][4];
-
-	for(int j = 0; j < truefaces; j++){
-	   intcoords[j][0] = atoi(coords[j][0]);
-	   intcoords[j][1] = atoi(coords[j][1]);
-		 intcoords[j][0] = atoi(coords[j][2]);
-		 intcoords[j][1] = atoi(coords[j][3]);
-	}
 
 	// 2. Load the Strong Classifier in a structure called `Cascade'
 	if( !cascade.load( cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
 
 	// 3. Detect Faces and Display Result
-	detectAndDisplay( frame, intcoords );
+	detectAndDisplay( frame );
 
 	// 4. Save Result Image
 	imwrite( "vj.jpg", frame );
 
 	std::cout.precision(5);
   std::cout.setf(std::ios::fixed);
-	std::cout << f_one() << std::endl;
+	//std::cout << f_one() << std::endl;
 
 	return 0;
 }
 
+int toInt( char x ){ return x;}
+
 float f_one(){
   int fp = truefaces - tp;
 	int fn = detected - tp;
-	float precision = tp/(tp+fp);
-  float recall = tp/(tp+fn);
-  float f1 = 2 * (precision * recall) / (precision + recall);
-  return f1;
+	//float precision = tp/(tp+fp);
+  // float recall = tp/(tp+fn);
+  // float f1 = 2 * (precision * recall) / (precision + recall);
+  // return f1;
 }
 
 /** @function detectAndDisplay */
-void detectAndDisplay( Mat frame, int** coordinates )
+void detectAndDisplay( Mat frame )
 {
 	std::vector<Rect> faces;
 	Mat frame_gray;
@@ -92,7 +117,7 @@ void detectAndDisplay( Mat frame, int** coordinates )
 	cascade.detectMultiScale( frame_gray, faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
 
   // 3. Print number of Faces found
-	std::cout << faces.size() << std::endl;
+	//std::cout << faces.size() << std::endl;
   detected = faces.size();
 
 
@@ -102,27 +127,39 @@ void detectAndDisplay( Mat frame, int** coordinates )
 		rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar( 0, 255, 0 ), 2);
   }
 
-	//5. calculate true positives
-  for(int j = 0; i < truefaces; j++)
+  //5. calculate true positives
+  //std::cout << truefaces << std::endl;
+  for(int i = 0; i < truefaces; i++)
 	{
 		int truematch = 0;
+    //std::cout << coordinates[i][i] << std::endl;
 		//iterate through true rectangles
 		//for each, iterate through detected rectangles
 		//if the detected rectangle is within the true rectangle, increment tp
 
-		for( int i = 0; i < faces.size(); i++ )
+		for( int j = 0; j < faces.size(); j++ )
 		{
-		   tl[2] = {faces[i].x, faces[i].y};
-			 br[2] = {faces[i].x + faces[i].width, faces[i].y + faces[i].height};
+		   int tl[2] = {faces[j].x, faces[j].y};
+			 int br[2] = {faces[j].x + faces[j].width, faces[j].y + faces[j].height};
 			 //if: detected rectangle's top left point is within true rectangle's top left Point
 			 //and detected rectangle's bottom right point is within true rectangle's bottom left point
-		 	 if( (coordinates[i][0] >= tl[0]) && (coordinates[i][1] <= tl[1]) && (coordinates[i][2] >= br[0]) && (coordinates[i][3] <= br[1]) )
-			 {
-				 truematch = 1;
-       }
-		}
+
+       if(coordinates[i][0] >= tl[0]){
+          std::cout << "match1" << std::endl;
+          if(coordinates[i][1] <= tl[1]){
+             std::cout << "match1" << std::endl;
+             if(coordinates[i][2] >= br[0]){
+                std::cout << "match3" << std::endl;
+                if(coordinates[i][3] <= br[1]){
+                   std::cout << "it worked" << std::endl;
+                   truematch = 1;
+
+      }}}}
+    }
+
 	if ( truematch == 1 ){
 		tp++;
 	}
 	}
+  	//std::cout << tp << std::endl;
 }
